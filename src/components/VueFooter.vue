014e770883fe3b14172d7cc65e3417a992351fc4
Jared Addison <drjaredaddison@gmail.com>
2021-08-22 01:58:56 +0000

    Footer

--- src/components/VueFooter.vue
+++ src/components/VueFooter.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
-    <span class="text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2021 © Anthony Fu</span>
+  <div class="mt-10 mb-6 prose m-auto opacity-50 flex text-center">
+    <span class="text-sm text-center">2021 © Jared Addison</span>
     <div class="flex-auto" />
+    <div class="mt-5 mx-auto text-center opacity-25 text-sm text-center">
+      Veterinary Education and Training
+    </div>
   </div>
 </template>
